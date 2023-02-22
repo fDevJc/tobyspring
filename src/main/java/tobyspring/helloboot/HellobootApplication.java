d14a3570c53efd13fb60a3bf62148acefd3a98fc
fDevJc <fdevjc@gmail.com>
2023-02-22 15:51:55 +0900

    헬로서블릿 추가

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,55 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		/*
+			org.apache.catalina.startup.Tomcat;
+			- 임베디드 톰캣을 제공
+			- 여러 설정정보를 넣어야됨
+			- 스프링부트에서 여러 설정정보를 기본적으로 세팅해주는 TomcatServletWebServerFactory가 존재
+		 */
+
+		//추상화
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		//ServletWebServerFactory jettyServletWebServerFactory = new JettyServletWebServerFactory();
+
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			//servlet container에 servlet을 등록, 매핑
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello Servlet" + name);
+
+				}
+			}).addMapping("/hello");
+		});
+
+		webServer.start();	//tomcat servlet container가 실행
+
+
 	}
 
 }
