2236cc17fb09fff580ba022658b029be1b0cf6d4
fDevJc <fdevjc@gmail.com>
2023-02-24 20:55:25 +0900

    @ConponentScan, @Component->@RestContoller, @Service

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,13 +3,12 @@ package tobyspring.helloboot;
 import java.util.Objects;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /*
 	Contoller의 역할은 검증
  */
-@RequestMapping
+@RestController
 public class HelloController {
 	private final HelloService helloService;
 
@@ -19,7 +18,6 @@ public class HelloController {
 
 	//
 	@GetMapping("/hello")
-	@ResponseBody
 	public String hello(String name) {
 		return helloService.sayHello(Objects.requireNonNull(name));
 	}
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,7 +3,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -14,18 +14,9 @@ import org.springframework.web.servlet.DispatcherServlet;
 			- 여러 설정정보를 넣어야됨
 			- 스프링부트에서 여러 설정정보를 기본적으로 세팅해주는 TomcatServletWebServerFactory가 존재
 */
-@Configuration //AnnotationConfigWebApplicationContext이 사용하는 빈으로 처음에 등록된다
+@Configuration
+@ComponentScan
 public class HellobootApplication {
-	@Bean
-	public HelloController helloController(HelloService helloService) {
-		return new HelloController(helloService);
-	}
-
-	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService();
-	}
-
 	public static void main(String[] args) {
 		//Spring Container
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
@@ -46,7 +37,7 @@ public class HellobootApplication {
 				webServer.start();    //tomcat servlet container가 실행
 			}
 		};
-		applicationContext.register(HellobootApplication.class);	//설정정보가 여기 있다
+		applicationContext.register(HellobootApplication.class);    //설정정보가 여기 있다
 		applicationContext.refresh();    //bean object 생성, templateMethod
 
 	}
