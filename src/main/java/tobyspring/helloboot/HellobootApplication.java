a68ed27e7a4108a7c7c2413d534fda2f87075698
fDevJc <fdevjc@gmail.com>
2023-02-24 20:09:45 +0900

    DispatcherServelt 적용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,19 +1,11 @@
 package tobyspring.helloboot;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
 /*
 			org.apache.catalina.startup.Tomcat;
 			- 임베디드 톰캣을 제공
@@ -24,10 +16,10 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		//Spring Container
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);
 		applicationContext.registerBean(SimpleHelloService.class);
-		applicationContext.refresh();	//bean object 생성
+		applicationContext.refresh();    //bean object 생성
 
 		//추상화
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
@@ -36,27 +28,9 @@ public class HellobootApplication {
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			//servlet container에 servlet을 등록, 매핑
 			//서블릿을 프론트컨트롤러 하나만 두고 공통처리인 인증,보안,다국어 처리등을 담당
-			servletContext.addServlet("frontController", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					//공통처리 로직
-					//...
-					//공통처리 로직
-					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {    //Mapping
-						String name = req.getParameter("name");    //파라미터 추출, 바인딩
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String ret = helloController.hello(name);
-
-						//응답
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*");
+			servletContext.addServlet("dispatcherServlet",
+				new DispatcherServlet(applicationContext)	//WebApplicationContext를 사용
+			).addMapping("/*");
 		});
 		webServer.start();    //tomcat servlet container가 실행
 	}
