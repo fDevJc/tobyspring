84ecf349aa23b08d6bfad936ae03c911f6625584
fDevJc <fdevjc@gmail.com>
2023-02-24 20:37:24 +0900

    빈 팩토리메소드 적용, AnnotationConfigWebApplicationContext로 수정하여 애노테이션을 통해 빈을 등록하는 스프링컨테이너로 변경

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -3,7 +3,9 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 /*
@@ -12,12 +14,22 @@ import org.springframework.web.servlet.DispatcherServlet;
 			- 여러 설정정보를 넣어야됨
 			- 스프링부트에서 여러 설정정보를 기본적으로 세팅해주는 TomcatServletWebServerFactory가 존재
 */
+@Configuration //AnnotationConfigWebApplicationContext이 사용하는 빈으로 처음에 등록된다
 public class HellobootApplication {
+	@Bean
+	public HelloController helloController(HelloService helloService) {
+		return new HelloController(helloService);
+	}
+
+	@Bean
+	public HelloService helloService() {
+		return new SimpleHelloService();
+	}
 
 	public static void main(String[] args) {
 		//Spring Container
 		//기존 GenericWebApplicationContext을 확장하여 onRefresh 메소드 오버라이딩
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext(){
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
@@ -29,16 +41,14 @@ public class HellobootApplication {
 					//servlet container에 servlet을 등록, 매핑
 					//서블릿을 프론트컨트롤러 하나만 두고 공통처리인 인증,보안,다국어 처리등을 담당
 					servletContext.addServlet("dispatcherServlet",
-						new DispatcherServlet(this)	//WebApplicationContext를 사용
+						new DispatcherServlet(this)    //WebApplicationContext를 사용
 					).addMapping("/*");
 				});
 				webServer.start();    //tomcat servlet container가 실행
 			}
 		};
-		applicationContext.registerBean(HelloController.class);
-		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.register(HellobootApplication.class);	//설정정보가 여기 있다
 		applicationContext.refresh();    //bean object 생성, templateMethod
-
 
 	}
 }
