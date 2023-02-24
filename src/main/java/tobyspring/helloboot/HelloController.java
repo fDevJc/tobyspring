37b6560e14bd5cb7e9056b40c9b6a16688f38307
fDevJc <fdevjc@gmail.com>
2023-02-24 20:20:04 +0900

    DispatcherServlet적용 -> Controller에 매핑정보, 응답정보 애노테이션 추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -2,9 +2,14 @@ package tobyspring.helloboot;
 
 import java.util.Objects;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /*
 	Contoller의 역할은 검증
  */
+@RequestMapping
 public class HelloController {
 	private final HelloService helloService;
 
@@ -12,6 +17,9 @@ public class HelloController {
 		this.helloService = helloService;
 	}
 
+	//
+	@GetMapping("/hello")
+	@ResponseBody
 	public String hello(String name) {
 		return helloService.sayHello(Objects.requireNonNull(name));
 	}
