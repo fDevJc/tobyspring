cc91f7a77983e404f8728bb74cef86c9ac26dae7
fDevJc <fdevjc@gmail.com>
2023-03-02 15:48:55 +0900

    커스텀 @Conditional-refactor

--- src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -9,20 +9,14 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.util.ClassUtils;
 
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
 	@Bean("jettyWebServerFactory")
 	public ServletWebServerFactory servletWebServerFactory() {
 		return new JettyServletWebServerFactory();
-	}
-
-	static class JettyCondition implements Condition {
-		@Override
-		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-			return ClassUtils.isPresent("org.eclipse.jetty.server.Server", context.getClassLoader());
-		}
 	}
 }
